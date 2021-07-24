a33be792f5ad0633a6452be06edf36b9a3af4cb7
سهيل هانى محمد صلاح الدين محمد فوزى <11410120170121@stud.cu.edu.eg>
2021-07-24 22:12:23 +0200

    unfinished

---
+++ Recommenders/ZoneRecommender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xZoneAPI.Models.Accounts;
+using xZoneAPI.Models.Zones;
+using xZoneAPI.Repositories.AccountRepo;
+using xZoneAPI.Repositories.ZoneRepo;
+
+namespace xZoneAPI.Recommenders
+{
+   
+    public class ZoneRecommender
+    {
+        IZoneRepository zoneRepo;
+        IZoneSkillRepository zoneSkillRepo;
+        IAccountSkillRepo accountSkillRepo;
+        public ZoneRecommender(IZoneRepository zoneRepo, IAccountSkillRepo accountSkillRepo, IZoneSkillRepository zoneSkillRepo)
+        {
+            this.zoneRepo = zoneRepo;
+            this.accountSkillRepo = accountSkillRepo;
+            this.zoneSkillRepo = zoneSkillRepo;
+        }
+        ICollection<Zone> getTenMaxRecommendedZones(ICollection<int> skillsIds, ICollection<Zone> zones)
+        {
+            foreach(Zone zone in zones)
+            {
+
+            }
+        }
+        
+        public ICollection<Zone> getRecommendedZones(int userId)
+        {
+            ICollection<int> skillsIds = accountSkillRepo.GetAccountSkillsId(userId);
+            ICollection<Zone> zones = zoneSkillRepo.GetZonesForSkill(skillsIds);
+            SortedDictionary<double, Zone> openWith =
+           new SortedDictionary<double, Zone>();
+        }
+    }
+}
